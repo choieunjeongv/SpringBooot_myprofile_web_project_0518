69e00544d65d1c8d9819971afa85597784563acb
choieunheong <mjchoi17@hanmail.net>
2022-05-19 11:14:54 +0900

    SpringBooot_myprofile_web_project

---
+++ src/main/java/com/javaeunjeong/myprofile_web/SpringBootMyprofileProject123Application.java
@@ -0,0 +1,13 @@
+package com.javaeunjeong.myprofile_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyprofileProject123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyprofileProject123Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaeunjeong/myprofile_web/SpringBootMyprofileProject123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaeunjeong.myprofile_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMyprofileProject123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
